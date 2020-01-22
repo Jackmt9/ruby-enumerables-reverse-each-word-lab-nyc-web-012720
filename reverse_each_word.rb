69721b3e098af04ba4f8940311acdbff4e3abcd7
Jack T <jackmt9@gmail.com>
2020-01-22 16:27:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word_with_each(string)
-  array = []
-  string.each do |word|
-    array << word.reverse
-  end
-  return array.join(" ")
-end
+# def reverse_each_word_with_each(string)
+#   array = []
+#   string.each do |word|
+#     array << word.reverse
+#   end
+#   return array.join(" ")
+# end
